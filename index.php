f6eb9ec10171cebb7b165d5c1ad6c48551ad5b6e
Anniek <anniekgithub@gmail.com>
2022-06-12 17:08:40 +0200

    worked on header

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     </div>
 </div>
 </header>
-<section class="has-mg-y">
+<section class="has-mg-y featured-products">
     <div class="container">
         <h2 class="section-title">Featured Products</h2>
         <p class="section-sub-title">- Our most wanted items -</p>
@@ -24,7 +24,7 @@
                 </div>
             </div>
             <div class="product">
-                <img class="product-img" src="img/sleeping-bag.jpg" alt="">
+                <img class="product-img" src="img/hammock.jpg" alt="">
                 <div class="product-desc">
                     <span class="product-brand">Brand</span>
                     <h5 class="product-name">Product Name</h5>
@@ -35,7 +35,7 @@
                 </div>
             </div>
             <div class="product">
-                <img class="product-img" src="img/sleeping-bag.jpg" alt="">
+                <img class="product-img" src="img/cooking.jpg" alt="">
                 <div class="product-desc">
                     <span class="product-brand">Brand</span>
                     <h5 class="product-name">Product Name</h5>
@@ -46,7 +46,7 @@
                 </div>
             </div>
             <div class="product">
-                <img class="product-img" src="img/sleeping-bag.jpg" alt="">
+                <img class="product-img" src="img/hammock.jpg" alt="">
                 <div class="product-desc">
                     <span class="product-brand">Brand</span>
                     <h5 class="product-name">Product Name</h5>
@@ -57,7 +57,7 @@
                 </div>
             </div>
             <div class="product">
-                <img class="product-img" src="img/sleeping-bag.jpg" alt="">
+                <img class="product-img" src="img/tent.jpg" alt="">
                 <div class="product-desc">
                     <span class="product-brand">Brand</span>
                     <h5 class="product-name">Product Name</h5>
@@ -82,6 +82,112 @@
     </div>
 
 </section>
+
+<section class="cta has-mg-y">
+    <h2 class="cta-title">Get 10% off when you buy a hammock</h2>
+    <h4>Use our code <span class="cta-code">HAMMOCK</span></h4>
+    <a href="#" class="btn btn-primary">Buy now</a>
+</section>
+
+<section class="has-mg-y new-arrivals">
+    <div class="container">
+        <h2 class="section-title">New arrivals</h2>
+        <p class="section-sub-title">- Get your fresh gear -</p>
+        <div class="products">
+            <div class="product">
+                <img class="product-img" src="img/sleeping-bag.jpg" alt="">
+                <div class="product-desc">
+                    <span class="product-brand">Brand</span>
+                    <h5 class="product-name">Product Name</h5>
+                    <div class="product-price-cart-wrapper">
+                        <h4 class="product-price">$79</h4>
+                        <a href="#"><img class="product-cart" src="/img/cart.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <img class="product-img" src="img/hammock.jpg" alt="">
+                <div class="product-desc">
+                    <span class="product-brand">Brand</span>
+                    <h5 class="product-name">Product Name</h5>
+                    <div class="product-price-cart-wrapper">
+                        <h4 class="product-price">$79</h4>
+                        <a href="#"><img class="product-cart" src="/img/cart.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <img class="product-img" src="img/cooking.jpg" alt="">
+                <div class="product-desc">
+                    <span class="product-brand">Brand</span>
+                    <h5 class="product-name">Product Name</h5>
+                    <div class="product-price-cart-wrapper">
+                        <h4 class="product-price">$79</h4>
+                        <a href="#"><img class="product-cart" src="/img/cart.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <img class="product-img" src="img/hammock.jpg" alt="">
+                <div class="product-desc">
+                    <span class="product-brand">Brand</span>
+                    <h5 class="product-name">Product Name</h5>
+                    <div class="product-price-cart-wrapper">
+                        <h4 class="product-price">$79</h4>
+                        <a href="#"><img class="product-cart" src="/img/cart.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <img class="product-img" src="img/tent.jpg" alt="">
+                <div class="product-desc">
+                    <span class="product-brand">Brand</span>
+                    <h5 class="product-name">Product Name</h5>
+                    <div class="product-price-cart-wrapper">
+                        <h4 class="product-price">$79</h4>
+                        <a href="#"><img class="product-cart" src="/img/cart.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+            <div class="product">
+                <img class="product-img" src="img/sleeping-bag.jpg" alt="">
+                <div class="product-desc">
+                    <span class="product-brand">Brand</span>
+                    <h5 class="product-name">Product Name</h5>
+                    <div class="product-price-cart-wrapper">
+                        <h4 class="product-price">$79</h4>
+                        <a href="#"><img class="product-cart" src="/img/cart.svg" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="has-mg-y banners">
+    <div class="container">
+        <div class="banners-sm">
+            <div class="banner-box">
+                <h2>heading</h2>
+                <p>paragraph</p>
+                <a class="btn btn-primary">Shop now</a>
+            </div>
+            <div class="banner-box">
+                <h2>heading</h2>
+                <p>paragraph</p>
+                <a class="btn btn-primary">Shop now</a>
+            </div>
+        </div>
+        <div class="banners-large">
+            <div class="banner-box-lg">
+                <h2>Using out gear?</h2>
+                <p>-Let us know on instagram-</p>
+                <img class="social-icon" src="img/icon-instagram.svg" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <?php include 'footer.php'; ?>
 </body>

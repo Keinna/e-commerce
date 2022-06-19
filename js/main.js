705e89802b00eddc8882c8a67d42ef97f927e461
Anniek <anniekgithub@gmail.com>
2022-06-19 13:12:54 +0200

    added hamburger button mobile

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+const navSlide = () => {
+    const burger = document.querySelector('.mobile-burger')
+    const nav = document.querySelector('.nav-items')
+    const navItems = document.querySelectorAll('.nav-items li')
+    //toggle nav
+    burger.addEventListener('click', () =>{
+        nav.classList.toggle('nav-active');
+
+            //animate items. Need index for delay animation (0.3 is initial delay)
+    navItems.forEach((link,index) =>{
+        if(link.style.animation){
+            link.style.animation = '';
+        }else{
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+        }
+    })
+    //burger animation
+    burger.classList.toggle('toggle')
+    })
+}
+
+navSlide();
+
 let products = document.getElementById('products')
 
 //update amount in cart nav + get data || nothing if there is no data
@@ -89,5 +112,6 @@ let calculateItemsCart = () => {
 
 // everytime app loads function will run
 calculateItemsCart();
+
 
 
